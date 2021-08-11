abadad090b29293562b51735ee7e17f6a4cc3529
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2021-08-11 11:26:38 +0800

    Add files via upload

---
+++ linux_amd64/main.py
@@ -0,0 +1,44 @@
+from selenium.webdriver import PhantomJS
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from random import randint
+from time import sleep
+
+
+def in_url(device: any, url: str):
+    if url in device.current_url():
+        return True
+    else:
+        return False
+
+
+def main():
+    urls = [
+        'https://down.fruitpan.com/fs/0c7o6dce5reaef342154fsgdfh3/',
+        'https://down.fruitpan.com/fs/8cbo0d1ebr2aaf942154fsgdfh9/',
+        'https://down.fruitpan.com/fs/ccodaeraaf42b154cfsgcdfhabb/',
+        'https://down.fruitpan.com/fs/ecod7era8f42f1544fsg7dfh8bb/'
+    ]
+    br.get(urls[randint(0, 3)])
+    WebDriverWait(
+        br, 20, 0.2
+    ).until(EC.element_to_be_clickable(
+        (By.XPATH, '/html/body/div[7]/div[2]/div[1]/div[1]/div[2]/div/div[1]/button'))
+    )
+    br.find_element_by_xpath(
+        '/html/body/div[7]/div[2]/div[1]/div[1]/div[2]/div/div[1]/button'
+    ).click()
+    sleep(30)
+    WebDriverWait(br, 20, 0.2).until(in_url(br, 'https://down.fruitpan.com/file/'))
+    sleep(2)
+    br.find_element_by_xpath(
+        '/html/body/div[7]/div[2]/div[1]/div[2]/div[1]/button[1]'
+    ).click()
+    sleep(5)
+    return()
+
+
+if __name__ == '__main__':
+    br = PhantomJS()
+    main()
