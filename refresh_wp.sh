420742ab844ec24b0a15eb7582ceb2512fea7cfb
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2021-08-11 17:55:40 +0800

    Update refresh_wp.sh

--- refresh_wp.sh
+++ refresh_wp.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+
 sudo pip3 install selenium
 cd /home
 sudo mkdir refresh
 cd refresh
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo git clone https://github.com/coder-yunyi/fuzzy-garbanzo.git
-export PATH=$PATH:/home/refresh/fuzzy-garbanzo/linux_amd64
-cd ./fuzzy-garbanzo/linux_amd64
+sudo wget https://cdn.jsdelivr.net/gh/coder-yunyi/fuzzy-garbanzo@main/linux_amd64/main.py
+sudo wget https://cdn.jsdelivr.net/gh/coder-yunyi/fuzzy-garbanzo@main/linux_amd64/chromedriver
+export PATH=$PATH:/home/refresh
 sudo python3 main.py
