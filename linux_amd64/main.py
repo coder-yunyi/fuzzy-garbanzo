e10f0555ec2d7f5f25e699fecee801d2455507c4
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2021-08-11 14:41:00 +0800

    Add files via upload

--- linux_amd64/main.py
+++ linux_amd64/main.py
@@ -1,4 +1,4 @@
-from selenium.webdriver import PhantomJS
+from selenium.webdriver import Chrome
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -6,7 +6,7 @@ from random import randint
 from time import sleep
 
 
-def in_url(device: any, url: str):
+def in_url(device, url):
     if url in device.current_url():
         return True
     else:
@@ -21,19 +21,20 @@ def main():
         'https://down.fruitpan.com/fs/ecod7era8f42f1544fsg7dfh8bb/'
     ]
     br.get(urls[randint(0, 3)])
-    br.implicitly_wait(40)
+    br.implicitly_wait(30)    
     br.find_element_by_xpath(
-        '/html/body/div[7]/div[2]/div[1]/div[1]/div[2]/div/div[1]/button'
+        '//*[@id="speed-type-1"]/div/div[1]/button'
     ).click()
     sleep(30)
     WebDriverWait(br, 20, 0.2).until(in_url(br, 'https://down.fruitpan.com/file/'))
+    sleep(2)
     br.find_element_by_xpath(
-        '/html/body/div[7]/div[2]/div[1]/div[2]/div[1]/button[1]'
+        '//*[@id="downbtn"]'
     ).click()
     sleep(5)
     return()
 
 
 if __name__ == '__main__':
-    br = PhantomJS()
+    br = Chrome()
     main()
