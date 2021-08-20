d9f9492995aea111b6c9c2f2a75b03cf8b61be4a
coder-yunyi <82013865+coder-yunyi@users.noreply.github.com>
2021-08-20 13:29:21 +0800

    Update main.py

--- linux_amd64/main.py
+++ linux_amd64/main.py
@@ -9,15 +9,15 @@ from time import sleep
 
 def main():
     urls = [
-        'https://url59.ctfile.com/f/33248059-507113316-bca3d5',
-        'https://url59.ctfile.com/f/33248059-507113317-d44329',
-        'https://url59.ctfile.com/f/33248059-507113315-da9588',
-        'https://url59.ctfile.com/f/33248059-507113314-a1c90e',
-        'https://url59.ctfile.com/f/33248059-507113313-de9247',
-        'https://url59.ctfile.com/f/33248059-507113312-d1e285',
-        'https://url59.ctfile.com/f/33248059-507113311-71fa8e',
-        'https://url59.ctfile.com/f/33248059-507113310-1967f3',
-        'https://url59.ctfile.com/f/33248059-507113309-c2cbc8'
+        'https://47.56.193.8/f/33248059-507113316-bca3d5',
+        'https://47.56.193.8/f/33248059-507113317-d44329',
+        'https://47.56.193.8/f/33248059-507113315-da9588',
+        'https://47.56.193.8/f/33248059-507113314-a1c90e',
+        'https://47.56.193.8/f/33248059-507113313-de9247',
+        'https://47.56.193.8/f/33248059-507113312-d1e285',
+        'https://47.56.193.8/f/33248059-507113311-71fa8e',
+        'https://47.56.193.8/f/33248059-507113310-1967f3',
+        'https://47.56.193.8/f/33248059-507113309-c2cbc8'
     ]
     br.get(urls[randint(0, len(urls) - 1)])
     try:
